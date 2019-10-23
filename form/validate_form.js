b12c8cad50b7459b237e124d41decab6d0a4fefc
Alyona_Vepritskaya <o.veprutska@student.csn.khai.edu>
2019-10-23 20:23:50 +0300

    Lab 3 Added js, form page, validation Lab3 Step 4

--- form/validate_form.js
+++ form/validate_form.js
@@ -1,41 +1,39 @@
 let psw1 = "", psw2 = "", email = "", login = "";
 document.querySelector('#psw1').addEventListener('change', () => {
     psw1 = document.querySelector('#psw1').value;
-    incorrectValue(psw1, 5, '#psw1', "Пароль должен содержать больше 4 символов");
+    incorrectValue(psw1, 4, '#psw1');
 });
 document.querySelector('#psw2').addEventListener('change', () => {
     psw2 = document.querySelector('#psw2').value;
-    incorrectValue(psw2, 5, '#psw2', "Пароль должен содержать больше 4 символов");
+    incorrectValue(psw2, 4, '#psw2');
 });
 document.querySelector('.submit')
     .addEventListener('click', () => {
-        if (psw1.length === 0 || email.length === 0 || login.length === 0) {
-            alert("Будьте внимательнее, заполните ВСЕ поля!!!");
+        if (document.getElementById('psw1').classList.contains('incorrect-input') ||
+            document.getElementById('psw2').classList.contains('incorrect-input') ||
+            document.getElementById('login').classList.contains('incorrect-input') ||
+            document.getElementById('email').classList.contains('incorrect-input') ||
+            psw1.length === 0 || psw2.length === 0 || login.length === 0 || email.length === 0) {
+            alert("Будьте внимательнее, допущена ошибка!!!");
         } else {
             (psw1 === psw2) ? alert("Ура, Вы зарегистрированы!!!") : alert("Будьте внимательнее, пароли не совпадают!!!");
         }
     });
 document.getElementById('email').addEventListener('change', () => {
     email = document.getElementById('email').value;
-    incorrectValue(email, emailIsValid(email), '#email');
+    incorrectValue(email, 0, '#email', !emailIsValid(email));
 });
 document.getElementById('login').addEventListener('change', () => {
     login = document.querySelector('#login').value;
-    if (login.length === 0) {
-        document.querySelector('#login').classList.add('incorrect-input');
-    } else {
-        document.querySelector('#login').classList.remove('incorrect-input');
-    }
+    incorrectValue(login, 0, '#login');
 });
 emailIsValid = (email) => {
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 };
-incorrectValue = (value, originValue, formId, innerText = '') => {
-    if ((value.length < originValue) || !originValue) {
+incorrectValue = (value, originValue, formId, emailValue = false) => {
+    if ((value.length <= originValue) || emailValue) {
         document.querySelector(formId).classList.add('incorrect-input');
-        document.querySelector('.help').textContent = innerText;
     } else {
         document.querySelector(formId).classList.remove('incorrect-input');
-        document.querySelector('.help').textContent = '';
     }
 };
