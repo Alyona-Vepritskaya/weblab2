0fdca6f7aed700eda904a7425fd59a5e92ecda45
Alyona_Vepritskaya <o.veprutska@student.csn.khai.edu>
2019-10-23 18:21:24 +0300

    Lab 3 Added js, form page, validation Lab3 Step 2

--- form/validate_form.js
+++ form/validate_form.js
@@ -2,13 +2,21 @@ let psw1 = "", psw2 = "", email = "", login = "";
 document.querySelector('#psw1').addEventListener('change', () => {
     psw1 = document.querySelector('#psw1').value;
     if (psw1.length < 8) {
-        alert('Value must be more than 8 !!!1');
+        document.querySelector('#psw1').classList.add('incorrect-input');
+        document.querySelector('.help').textContent = 'Пароль должен содержать больше 8 символов';
+    } else {
+        document.querySelector('#psw1').classList.remove('incorrect-input');
+        document.querySelector('.help').textContent = '';
     }
 });
 document.querySelector('#psw2').addEventListener('change', () => {
     psw2 = document.querySelector('#psw2').value;
     if (psw2.length < 8) {
-        alert('Value must be more than 8 !!!2');
+        document.querySelector('#psw2').classList.add('incorrect-input');
+        document.querySelector('.help').textContent = 'Пароль должен содержать больше 8 символов';
+    } else {
+        document.querySelector('#psw2').classList.remove('incorrect-input');
+        document.querySelector('.help').textContent = '';
     }
 });
 document.querySelector('.submit')
@@ -25,13 +33,21 @@ document.querySelector('.submit')
     });
 document.getElementById('email').addEventListener('change', () => {
     email = document.getElementById('email').value;
-    if (!emailIsValid(email)) {
-        alert('Invalid email');
+    if (!emailIsValid(email) || email.length === 0) {
+        document.querySelector('#email').classList.add('incorrect-input');
+    } else {
+        document.querySelector('#email').classList.remove('incorrect-input');
     }
+
 });
 document.getElementById('login').addEventListener('change', () => {
     login = document.querySelector('#login').value;
+    if (login.length === 0) {
+        document.querySelector('#login').classList.add('incorrect-input');
+    } else {
+        document.querySelector('#login').classList.remove('incorrect-input');
+    }
 });
 emailIsValid = (email) => {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 };
