d3ea852062e7b1e876b54d0d6e5cd0b680b002e7
Alyona_Vepritskaya <o.veprutska@student.csn.khai.edu>
2019-10-25 12:13:13 +0300

    Lab 3 Added js, form page, validation ,redirect, animation Lab3 Step 9

--- animation/animation.js
+++ animation/animation.js
@@ -1,12 +1,3 @@
-/*
-/!*document.querySelector('.item').addEventListener('mouseenter',()=>{
-    document.querySelector('.item').style.transform = "translate(0px,5px)";
-});*!/
-document.querySelector('.item').addEventListener('mouseleave',()=>{
-    document.querySelector('.item').style.transform = "translate(0px,0px)";
-});
-*/
-
 document.querySelectorAll('.item').forEach(value => {
    value.addEventListener('mouseenter',()=>{
         value.style.transform = "translate(8px,0px)";
