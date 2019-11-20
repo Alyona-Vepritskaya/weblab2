fd02f30b48e3e1f43c925c5503afde4d9b4e2696
Alyona_Vepritskaya <alena0112358132134@gmail.com>
2019-11-20 22:35:33 +0200

    Lab 4  release 4

--- form_on_php/main.php
+++ form_on_php/main.php
@@ -13,28 +13,28 @@
         <div class="form">
             <form action="validate_form.php" class="qwerty" method="post">
                 <div id="pain">
-                    <?php if ($error != '') { ?>
-                        <?php echo $error ?>
-                    <?php } ?>
+                    <?php if ($error != '') {
+                        echo $error;
+                    } ?>
                 </div>
                 <div class="block">
                     Логин:
                     <input id="login" type="text" name="login"
-                           value="<?php if ($error != '') echo htmlspecialchars($login) ?>">
+                           value="<?php if ($error != '') echo $login ?>">
                 </div>
                 <div class="block">
                     Пароль:
                     <input id="psw1" type="password" name="psw1"
                            placeholder="введите пароль"
-                           value="<?php if ($error != '') echo htmlspecialchars($psw1) ?>">
+                           value="<?php if ($error != '') echo $psw1 ?>">
                     <input id="psw2" type="password" name="psw2"
                            placeholder="повторите пароль"
-                           value="<?php if ($error != '') echo htmlspecialchars($psw2) ?>">
+                           value="<?php if ($error != '') echo $psw2 ?>">
                 </div>
                 <div class="block">
                     Email:
                     <input id="email" type="text" name="email"
-                           value="<?php if ($error != '') echo htmlspecialchars($email) ?>">
+                           value="<?php if ($error != '') echo $email ?>">
                 </div>
                 <div class="block">
                     <input class="submit" type="submit">
