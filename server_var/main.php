3093f6e4699da3c3269cf9d840b3b9f7fbd1ecbd
Alyona_Vepritskaya <alena0112358132134@gmail.com>
2019-11-25 16:36:43 +0200

    Lab 4  release 6

--- server_var/main.php
+++ server_var/main.php
@@ -1,8 +1,14 @@
 <?php
+function check()
+{
+    if (filter_input_('h_input', 'first') == 'first') {
+        $_POST['h_input'] = 'second';
+        return false;
+    } else {
+        return true;
+    }
+}
 
-/*if (!isset($error)) {
-    $error = '';
-}*/
 function filter_input_($name, $def)
 {
     $result = $def;
@@ -39,53 +45,85 @@ if (!(isIncorrect($psw1, 3) && isIncorrect($psw2, 3) &&
     $error = '';
 }
 ?>
+
 <div class="right-col">
     <div class="news-info">
         <a href="">
             <div class="news">
-                Server
+               Server
             </div>
         </a>
         <div class="date">
-            30 февраля 666
+            30 февраля 1313
         </div>
     </div>
     <div class="text-content  clearfix">
         <div class="form">
-            <form  class="qwerty" method="post">
-                <!--  <input  type="text" name="h_input" value="reg" hidden="hidden">-->
-                <div id="pain">
-                    <?php if ($error != '') {
-                        echo $error;
-                    } ?>
+            <?php if ($error != '' || !check()) { ?>
+                <form class="qwerty" method="post" id="form">
+                    <input type="text" name="h_input" hidden="hidden">
+                    <div id="pain">
+                        <?php if ($error != '' && check()) {
+                            echo $error;
+                        } ?>
+                    </div>
+                    <div class="block">
+                        Логин:
+                        <input id="login" type="text" name="login"
+                               value="<?php if ($error != '') echo $login ?>">
+                    </div>
+                    <div class="block">
+                        Пароль:
+                        <input id="psw1" type="password" name="psw1"
+                               placeholder="введите пароль"
+                               value="<?php if ($error != '') echo $psw1 ?>">
+                        <input id="psw2" type="password" name="psw2"
+                               placeholder="повторите пароль"
+                               value="<?php if ($error != '') echo $psw2 ?>">
+                    </div>
+                    <div class="block">
+                        Email:
+                        <input id="email" type="text" name="email"
+                               value="<?php if ($error != '') echo $email ?>">
+                    </div>
+                    <div class="block">
+                        <input class="submit" type="submit">
+                    </div>
+                </form>
+            <?php } else { ?>
+                <div id="registered">
+                    Мои поздравления, ты зарегался!!! 🥳 и <br>
+                    сейчас увидишь значения серверных переменных
+                    <p>
+                        <!--Переменная $_SERVER - это массив, содержащий информацию, такую как заголовки,
+                        пути и местоположения скриптов. Записи в этом массиве создаются веб-сервером.-->
+                        $_SERVER
+                        <?php print("<pre>" . print_r($_SERVER, true) . "</pre>"); ?>
+                        <!--Переменные HTTP POST-->
+                        $_POST
+                        <?php print("<pre>" . print_r($_POST, true) . "</pre>"); ?>
+                        <!--Переменные HTTP GET-->
+                        $_GET
+                        <?php print("<pre>" . print_r($_GET, true) . "</pre>"); ?>
+                        <!--Переменные файлов, загруженных по HTTP-->
+                        $_FILES
+                        <?php print("<pre>" . print_r($_FILES, true) . "</pre>"); ?>
+                        <!--Ассоциативный массив (array) значений, переданных скрипту через HTTP Cookies.-->
+                        <!--Куки (англ. Cookie) — это текстовый файл с данными, который записывается в браузер,
+                        сервером посещаемого вами сайта. Этими данными являются:
+                        * информация о логине и пароле;
+                        * индивидуальные настройки и предпочтения пользователя;
+                        * статистика посещений и т.д.-->
+                        $_COOKIE
+                        <?php print("<pre>" . print_r($_COOKIE, true) . "</pre>"); ?>
+                        <!--Переменные окружения-->
+                        $_ENV
+                        <?php print("<pre>" . print_r($_ENV, true) . "</pre>"); ?>
+                        <!--Ссылки на все переменные глобальной области видимости-->
+                        $GLOBALS
+                        <?php print("<pre>" . print_r($GLOBALS, true) . "</pre>"); ?>
+                    </p>
                 </div>
-                <div class="block">
-                    Логин:
-                    <input id="login" type="text" name="login"
-                           value="<?php if ($error != '') echo $login ?>">
-                </div>
-                <div class="block">
-                    Пароль:
-                    <input id="psw1" type="password" name="psw1"
-                           placeholder="введите пароль"
-                           value="<?php if ($error != '') echo $psw1 ?>">
-                    <input id="psw2" type="password" name="psw2"
-                           placeholder="повторите пароль"
-                           value="<?php if ($error != '') echo $psw2 ?>">
-                </div>
-                <div class="block">
-                    Email:
-                    <input id="email" type="text" name="email"
-                           value="<?php if ($error != '') echo $email ?>">
-                </div>
-                <div class="block">
-                    <input class="submit" type="submit">
-                </div>
-            </form>
-
+            <?php } ?>
         </div>
-        <p>
-            <?php print("<pre>" . print_r($_SERVER, true) . "<pre>"); ?>
-            <?php print("<pre>" . print_r($_POST, true) . "</pre>"); ?>
-            <?php print("<pre>" . print_r($_GET, true) . "</pre>"); ?>
-        </p>
+    </div>
