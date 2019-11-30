58b7c89b780ae9e4dfc80f0d832d158f584333b2
Alyona_Vepritskaya <alena0112358132134@gmail.com>
2019-11-30 17:36:29 +0200

    Lab 4  release 10 added values of server variables

--- server_var/main.php
+++ server_var/main.php
@@ -12,35 +12,53 @@
     <div class="text-content  clearfix">
         <div id="registered">
             Значения серверных переменных
-            <p>
-                <!--Переменная $_SERVER - это массив, содержащий информацию, такую как заголовки,
-                пути и местоположения скриптов. Записи в этом массиве создаются веб-сервером.-->
-                $_SERVER
-                <?php print("<pre>" . print_r($_SERVER, true) . "</pre>"); ?>
-                <!--Переменные HTTP POST-->
-                $_POST
-                <?php print("<pre>" . print_r($_POST, true) . "</pre>"); ?>
-                <!--Переменные HTTP GET-->
-                $_GET
-                <?php print("<pre>" . print_r($_GET, true) . "</pre>"); ?>
-                <!--Переменные файлов, загруженных по HTTP-->
-                $_FILES
-                <?php print("<pre>" . print_r($_FILES, true) . "</pre>"); ?>
-                <!--Ассоциативный массив (array) значений, переданных скрипту через HTTP Cookies.-->
-                <!--Куки (англ. Cookie) — это текстовый файл с данными, который записывается в браузер,
-                сервером посещаемого вами сайта. Этими данными являются:
-                * информация о логине и пароле;
-                * индивидуальные настройки и предпочтения пользователя;
-                * статистика посещений и т.д.-->
-                $_COOKIE
-                <?php print("<pre>" . print_r($_COOKIE, true) . "</pre>"); ?>
-                <!--Переменные окружения-->
-                $_ENV
-                <?php print("<pre>" . print_r($_ENV, true) . "</pre>"); ?>
-                <!--Ссылки на все переменные глобальной области видимости-->
-                $GLOBALS
-                <?php print("<pre>" . print_r($GLOBALS, true) . "</pre>"); ?>
-            </p>
+            <br>
+            <table class="server">
+                <tr>
+                    <td class="vars">$_GET</td>
+                    <td></td>
+                </tr>
+                <?php
+                foreach($_GET as $key => $value){
+                    echo "<tr><td>$key</td>";
+                    echo "<td>$value</td></tr>";
+
+                }?>
+                <tr>
+                    <td class="vars">$_POST</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <?php
+                    foreach($_POST as $key => $value){
+                        echo "<tr><td>$key</td>";
+                        echo "<td>$value</td></tr>";
+
+                    }?>
+                </tr>
+                <tr>
+                    <td class="vars">$_SERVER</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <?php
+                    foreach($_SERVER as $key => $value){
+                        echo "<tr><td>$key</td>";
+                        echo "<td>$value</td></tr>";
+                    }?>
+                </tr>
+                <tr>
+                    <td class="vars">$_COOKIE</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <?php
+                    foreach($_COOKIE as $key => $value){
+                        echo "<tr><td>$key</td>";
+                        echo "<td>$value</td></tr>";
+                    }?>
+                </tr>
+            </table>
         </div>
     </div>
 </div>
