ffd3a2edec6f67f3a79bb2ab17b76f8ad90efa22
Alyona_Vepritskaya <alena0112358132134@gmail.com>
2019-12-13 21:29:13 +0200

    Lab 5  release 13

--- nav_dir/index.php
+++ nav_dir/index.php
@@ -1,8 +1,7 @@
 <?php
-
 /**
  * @param $size - in bytes
- * @return string - size in kb, mb, gb
+ * @return string - size in b, kb, mb, gb
  */
 function convertSize($size)
 {
@@ -23,44 +22,12 @@ function convertSize($size)
     return round($new_size, 2) . ' ' . $units;
 }
 
-function sortDirs($dirs, $path)
-{
-    $directories = array();
-    foreach ($dirs as $key => $value) {
-        if (is_dir($path . $value)) {
-            $directories[] = $dirs[$key];
-        }
-    }
-    //natcasesort - sort an array using a case insensitive algorithm
-    natcasesort($directories);
-    return $directories;
-}
-
-function sortFiles($dirs, $path)
-{
-    $files = array();
-    foreach ($dirs as $key => $value) {
-        if (is_file($path . $value)) {
-            $files[] = $dirs[$key];
-        }
-    }
-    //natcasesort - sort an array using a case insensitive algorithm
-    natcasesort($files);
-    return $files;
-}
-
 function filter_input_($name, $default)
 {
     $result = $default;
     if (isset($_POST[$name])) {
         $result = $_POST[$name];
     }
-    /*if (isset($_SERVER[$name])) {
-        $result = $_SERVER[$name];
-    }
-    if (isset($_FILES[$name])) {
-        $result = $_FILES[$name];
-    }*/
     if (isset($_GET[$name])) {
         $result = $_GET[$name];
     }
@@ -69,21 +36,18 @@ function filter_input_($name, $default)
 
 /**
  * @param $path - directory(if is dir)
- * @return void - content(files & dirs)
+ * @return void
+ * shares, sorts, gets data about  dirs & files
  */
 function openDirectory($path)
 {
-    global $directories;
-    global $files;
-    global $dir_date;
-    global $file_date;
-    global $file_size;
+    global $directories, $files, $dir_date, $file_date, $file_size;
     $inner_dirs = array();
     if (is_dir($path)) {
         if ($d = opendir($path)) {
             while (($file = readdir($d)) !== false) {
                 $inner_dirs[] = $file;
-                (is_file($path.$file)) ? $files[] = $file : $directories[] = $file;
+                (is_file($path . $file)) ? $files[] = $file : $directories[] = $file;
             }
             closedir($d);
         }
@@ -91,38 +55,12 @@ function openDirectory($path)
     natcasesort($files);
     natcasesort($directories);
     foreach ($files as $key => $value) {
-        $file_date[] = date('Y.m.d _ H:i:s', filectime($path.$value));
-        $file_size[] = convertSize(filesize($path.$value));
+        $file_date[] = date('Y.m.d _ H:i:s', filectime($path . $value));
+        $file_size[] = convertSize(filesize($path . $value));
     }
     foreach ($directories as $key => $value) {
-        $dir_date[] = date('Y.m.d _ H:i:s', filectime($path.$value));
+        $dir_date[] = date('Y.m.d _ H:i:s', filectime($path . $value));
     }
-}
-
-/**
- * @param $dirs - array of directories
- * @param $path - path to file or dir
- * @return array - creation dates
- */
-function getFileDirDate($dirs, $path)
-{
-    $file_date = array();
-    foreach ($dirs as $key => $value) {
-        $file_date[$key] = date('Y.m.d _ H:i:s', filectime($path . $value));
-    }
-    return $file_date;
-}
-
-function getFileSize($dirs, $path)
-{
-    $file_size = array();
-    foreach ($dirs as $key => $value) {
-        if (is_file($path . $value))
-            $file_size[$key] = convertSize(filesize($path . $value));
-        else
-            $file_size[$key] = ' - ';
-    }
-    return $file_size;
 }
 
 function loadFile()
@@ -143,28 +81,25 @@ function loadFile()
     }
 }
 
+//TODO HARDCODE!!!!
 function find_path($value)
 {
-    /*__DIR__*/
     global $path;
-    global $current_dir;
-    if ($value != '.') {
-        $path_get = $path . $value . '/';
+    if ($value != '..') {
+        return substr($path, 16, strlen($path) - 1) . $value;
     } else {
-        if ($path == "../") {
-            $path_get = "../" . $value . '/';
-        } else {
-            $tmp = substr($current_dir, 0, strlen($current_dir) - 2);
-            $tmp2 = strrpos($tmp, '/');
-            $final = substr($tmp, 0, $tmp2 + 1);
-            if ($tmp2 != 2) {
-                $path_get = $final;
-            } else {
-                $path_get = "../";
-            }
-        }
+        $tmp = substr((substr($path, 16, strlen($path) - 1) . $value), 0, strlen($path) - 1);
+        $tmp2 = strrpos($tmp, '\\');
+        $final = substr($tmp, 0, $tmp2);
+        $tmp3 = strrpos($final, '\\');
+        $final2 = substr($final, 0, $tmp3);
+        return $final2;
     }
-    return $path_get;
+}
+
+function cutEnd($srt)
+{
+    return substr($srt, 0, strrpos($srt, '\\') + 1);
 }
 
 $directories = array();
@@ -173,21 +108,16 @@ $file_date = array();
 $dir_date = array();
 $file_size = array();
 
-if (filter_input_('get_request', 'no_request') == 'no_request') {
-    $dir = filter_input_("DOCUMENT_ROOT", '');
-    $path = '../';
-    $current_dir = $path;
-} else {
+$path = cutEnd(__DIR__);
+if (filter_input_('get_request', 'no_request') != 'no_request') {
     $dir = filter_input_('get_request', '');
-    $path = $dir;
+    if (!(strrpos($dir, '..') === false && strrpos($dir, '\\\\') === false && strrpos($dir, '//') === false))
+        $path = cutEnd(__DIR__);
+    else
+        $path .= ($dir . '\\');
     $current_dir = $path;
 }
-$dirs = openDirectory($path);
-/*$directories = sortDirs($dirs, $path);
-$files = sortFiles($dirs, $path);*/
-/*$file_size = getFileSize($files, $path);
-$file_date = getFileDirDate($files, $path);
-$dir_date = getFileDirDate($directories, $path);*/
+openDirectory($path);
 loadFile();
 
 include "../general/header.php"; ?>
@@ -212,11 +142,13 @@ include "../general/header.php"; ?>
                     <th>Date</th>
                 </tr>
                 <?php foreach ($directories as $key => $value) {
-                    $path_get = find_path($value);
-                    echo "<tr><td><a href='index.php?get_request=$path_get'>$value</a></td>";
-                    echo "<td>dir</td>";
-                    echo "<td>-</td>";
-                    echo "<td>$dir_date[$key]</td></tr>";
+                    if ($value != '.') {
+                        $path_get = find_path($value);
+                        echo "<tr><td><a href='index.php?get_request=$path_get'>$value</a></td>";
+                        echo "<td>dir</td>";
+                        echo "<td>-</td>";
+                        echo "<td>$dir_date[$key]</td></tr>";
+                    }
                 } ?>
                 <?php foreach ($files as $key => $value) {
                     echo "<tr><td>$value</td>";
