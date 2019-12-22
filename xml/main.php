ba060002227a8a6fa3e5ffaf5830adfa254de732
Alyona_Vepritskaya <alena0112358132134@gmail.com>
2019-12-22 16:32:24 +0200

    Lab 6  release 5

--- xml/main.php
+++ xml/main.php
@@ -1,4 +1,5 @@
 <?php
+$products = array();
 $items = array();
 $item = array();
 function parseData()
@@ -36,10 +37,11 @@ function parseData()
  */
 function start($parser, $name, $attribs)
 {
-    global $item;
+    global $item, $itemsType, $items;
     switch ($name) {
         case "ITEMS":
-            $item["TYPE"] = $attribs["TYPE"];
+            $items = array();
+            $itemsType = $attribs["TYPE"];
             break;
         case "SOME_PARAM":
             $item["pName"] = $attribs["NAME"];
@@ -52,12 +54,16 @@ function start($parser, $name, $attribs)
 
 function stop($parser, $element_name)
 {
-    global $item, $items, $currentData;
-    if ($element_name != "ITEM" && $element_name != "SOME_PARAM" && $element_name != "ITEMS") {
+
+    global $item, $items, $currentData, $itemsType, $products;
+    if ($element_name != "ITEM" && $element_name != "SOME_PARAM" && $element_name != "PRODUCTS" && $element_name != "ITEMS") {
         $item[$element_name] = $currentData;
     }
     if ($element_name == "ITEM") {
         $items[] = $item;
+    }
+    if ($element_name == "ITEMS") {
+        $products[$itemsType] = $items;
     }
 }
 
@@ -88,23 +94,11 @@ function data($parser, $data)
             <?php
             $currentType = '';
             parseData();
-            foreach ($items as $item) {
-                if ($item['TYPE'] != $currentType) { ?>
-                    <divclass="item-name"><?=strtoupper($item['TYPE'][0]).substr($item['TYPE'], 1)?></div>
-                    <?php
-                    $currentType = $item['TYPE'];
+            foreach ($products as $items => $item) {
+                echo $items;
+                foreach ($item as $key => $value) {
+                    echo $value['NAME'] . "<br>";
                 }
-                ?>
-                <div class="product">
-                    <div class="item-name"><?=$item['NAME']?></div>
-                    <img src="<?=$item['IMAGE']?>" alt="img">
-                    <div>Serial number: <?=$item['ID']?></div>
-                    <div>Price: <?=$item['PRICE']?></div>
-                    <div>Production date: <?=$item['PROD_YEAR']?></div>
-                    <div>Production country: <?=$item['PROD_COUNTRY']?></div>
-                    <div><?=strtoupper($item['pName'][0]) . substr($item['pName'], 1)?>: <?=$item['pValue']?></div>
-                </div>
-                <?php
             }
             ?>
         </div>
