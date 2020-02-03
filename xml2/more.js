f0cafc05843a498fdcc7c0f77f8d9a22a58d7798
Alyona_Vepritskaya <alena0112358132134@gmail.com>
2020-02-03 16:26:58 +0200

    Lab 6 release 16.

--- xml2/more.js
+++ xml2/more.js
@@ -15,7 +15,7 @@ document.querySelectorAll('.more').forEach(item => {
             };
             xmlhttp.open("GET", "getMoreInfo.php?item=" + event.target.parentNode.attributes.getNamedItem('s_num').value, true);
             xmlhttp.send();
-            event.target.value = 'Buy Now';
+            event.target.value = 'Buy';
         }
     })
 });
