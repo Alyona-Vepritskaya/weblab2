cd78f225e16dde059c9286d9fe8393d4612d4727
Alyona_Vepritskaya <amusing.arctic.fox@gmail.com>
2020-02-11 17:47:56 +0200

    lab8 release v2

--- jquery_form/validate_form_jquery.js
+++ jquery_form/validate_form_jquery.js
@@ -1,8 +1,7 @@
 $(document).ready(
     function () {
         $('#all').click(function () {
-            $("input[type=checkbox]").prop('checked', true);
-
+            $("input[type=checkbox]").prop('checked', true)
         });
         $('#no').click(function () {
             $("input[type=checkbox]").prop('checked', false);
@@ -18,39 +17,40 @@ $(document).ready(
                 const psw2 = $('#psw2').val().trim();
                 const email = $('#email').val();
                 if (!(isIncorrect(psw1, 4) && isIncorrect(psw2, 4) &&
-                    isIncorrect(login, 0) && emailIsValid(email))) {
-                    incorrectValue(login, 0, '#login');
+                    isIncorrect(login, 3) && emailIsValid(email))) {
+                    incorrectValue(login, 3, '#login');
                     incorrectValue(psw1, 4, '#psw1');
                     incorrectValue(psw2, 4, '#psw2');
-                    incorrectValue(email, 4, '#email');
+                    incorrectValue(email, 4, '#email', !emailIsValid(email));
                     event.preventDefault();
                 } else {
-                    if (psw1 !== psw2) {
+                    if (psw1 != psw2) {
                         $('#psw2').next().text('Passwords don\'t match !!!');
                         $('#psw2').addClass('incorrect-input');
                         $('#psw1').addClass('incorrect-input');
+                        $('#psw2').val('');
+                        $('#psw1').val('');
                         event.preventDefault();
                     }
                 }
             }
         );
-        $('#login').change(function () {
+        $('#login').focusout(function () {
             const login = $('#login').val().trim();
-            incorrectValue(login, 4, '#login');
+            incorrectValue(login, 3, '#login');
         });
-        $('#psw1').change(function () {
-            const login = $('#psw1').val().trim();
-            incorrectValue(login, 4, '#psw1');
+        $('#psw1').focusout(function () {
+            const psw1 = $('#psw1').val().trim();
+            incorrectValue(psw1, 4, '#psw1');
         });
-        $('#psw2').change(function () {
-            const login = $('#psw2').val().trim();
-            incorrectValue(login, 4, '#psw2');
+        $('#psw2').focusout(function () {
+            const psw2 = $('#psw2').val().trim();
+            incorrectValue(psw2, 4, '#psw2');
         });
-        $('#email').change(function () {
-            const login = $('#email').val().trim();
-            incorrectValue(login, 4, '#email');
+        $('#email').focusout(function () {
+            const email = $('#email').val().trim();
+            incorrectValue(email, 4, '#email', !emailIsValid(email));
         });
-
     });
 
 function incorrectValue(value, originValue, formId, emailValue = false) {
@@ -58,14 +58,14 @@ function incorrectValue(value, originValue, formId, emailValue = false) {
         $(formId).addClass('incorrect-input');
         switch (formId) {
             case '#login':
-                $(formId).next().text('Login must have at least 3 character');
+                $(formId).next().text('Login must have at least 4 character');
                 break;
             case '#email':
                 $(formId).next().text('Incorrect email format');
                 break;
             case '#psw2':
             case 'psw1':
-                $(formId).next().text('Password must have at least 3 character');
+                $(formId).next().text('Password must have at least 5 character');
                 break;
             default:
                 break;
@@ -74,7 +74,6 @@ function incorrectValue(value, originValue, formId, emailValue = false) {
         $(formId).removeClass('incorrect-input');
         $(formId).next().text('');
     }
-
 }
 
 function emailIsValid(email) {
