09aec1cdc86134524afece44574cd948e5a5c513
Alyona_Vepritskaya <amusing.arctic.fox@gmail.com>
2020-03-17 15:17:29 +0200

    Lab10 release v11

--- resize_img/index.php
+++ resize_img/index.php
@@ -1,8 +1,6 @@
 <?php
 $error_message = null;
-$image_to_display = null;// !
-function filter_input_($name, $default)
-{
+function filter_input_($name, $default){
     $result = $default;
     if (isset($_POST[$name]))
         $result = $_POST[$name];
@@ -11,8 +9,7 @@ function filter_input_($name, $default)
     return $result;
 }
 
-function loadFile()
-{
+function loadFile(){
     global $error_message, $img_type;
     $submit = filter_input_('input_submit', '');
     if (!empty($submit) && (!empty($_FILES['file']['tmp_name']))) {
@@ -27,84 +24,84 @@ function loadFile()
         $image = null;
         switch ($img_type) {
             case 1:
+                echo 'gif';
                 $image = imagecreatefromgif($loaded_file['tmp_name']);
                 break;
             case 2:
+                echo 'jpeg';
                 $image = imagecreatefromjpeg($loaded_file['tmp_name']);
                 break;
             case 3:
+                echo 'png';
                 $image = imagecreatefrompng($loaded_file['tmp_name']);
                 break;
             default: // unsupported type
                 break;
         }
-        (is_null($image)) ? $error_message = 'Unsupported  image type' :
+        (is_null($image) || is_bool($image)) ? $error_message = 'Unsupported  image type' :
             crop_and_resize($image, $height, $width);
     }
 }
 
-function set_text_and_save($image_p)
-{
+function set_text_and_save($img){
     global $img_type;
+    echo $img_type;
     $text = "     Alyona\nVeprytskaya";
     $font = dirname(__FILE__) . '/ArialBlack.ttf';
-    imagettftext($image_p, 10, 0, 10, 20, imagecolorallocate($image_p, 245, 34, 109), $font, $text);
-    // header('Content-Type: image/jpeg'); //указываем на тип передаваемых данных
+    //Write text to the image using TrueType fonts
+    imagettftext($img, 10, 0, 10, 20, imagecolorallocate($img, 245, 34, 109), $font, $text);
     // Output the image
+    //header('Content-Type: image/png');//indicates the type of data transmitted
     switch ($img_type) {
         case 1:
-            imagegif($image_p, 'example-cropped.gif');
+            imagegif($img, 'example-cropped.gif');
             break;
         case 2:
-            imagejpeg($image_p, 'example-cropped.jpeg');
+            imagejpeg($img, 'example-cropped.jpeg');
             break;
         case 3:
-            imagepng($image_p, 'example-cropped.png');
+            imagepng($img,'example-cropped.png');
             break;
         default: // unsupported type
             break;
     }
     // Free up memory
-    imagedestroy($image_p);
+    imagedestroy($img);
 }
 
-function crop_width($image, $height, $width)
-{
-    global $cropWidth, $cropHeight;
-    $newCropWidth = round($height * $cropWidth / $cropHeight);
-    $shift = round(($width - $newCropWidth) / 2);
-    $im2 = imagecrop($image, ['x' => $shift, 'y' => 0, 'width' => $newCropWidth, 'height' => $height]);
-    $image_p = imagecreatetruecolor($cropWidth, $cropHeight);
-    imagecopyresampled($image_p, $im2, 0, 0, 0, 0, $cropWidth, $cropHeight, $newCropWidth, $height);
-    set_text_and_save($image_p);
+function crop_width($image, $height, $width){
+    global $crop_width, $crop_height;
+    $new_crop_width = round($height * $crop_width / $crop_height);
+    $shift = round(($width - $new_crop_width) / 2);
+    $cropped_image = imagecrop($image, ['x' => $shift, 'y' => 0, 'width' => $new_crop_width, 'height' => $height]);
+    $result_image = imagecreatetruecolor($crop_width, $crop_height);
+    imagecopyresampled($result_image, $cropped_image, 0, 0, 0, 0, $crop_width, $crop_height, $new_crop_width, $height);
+    set_text_and_save($result_image);
 }
 
-function crop_height($image, $height, $width)
-{
-    global $cropWidth, $cropHeight;
-    $newCropHeight = round($width * $cropHeight / $cropWidth);
-    $shift = round(($height - $newCropHeight) / 2);
-    $im2 = imagecrop($image, ['x' => 0, 'y' => $shift, 'width' => $width, 'height' => $newCropHeight]);
-    $image_p = imagecreatetruecolor($cropWidth, $cropHeight);
-    imagecopyresampled($image_p, $im2, 0, 0, 0, 0, $cropWidth, $cropHeight, $width, $newCropHeight);
-    set_text_and_save($image_p);
+function crop_height($image, $height, $width){
+    global $crop_width, $crop_height;
+    $new_crop_height = round($width * $crop_height / $crop_width);
+    $shift = round(($height - $new_crop_height) / 2);
+    $cropped_image = imagecrop($image, ['x' => 0, 'y' => $shift, 'width' => $width, 'height' => $new_crop_height]);
+    $result_image = imagecreatetruecolor($crop_width, $crop_height);
+    imagecopyresampled($result_image, $cropped_image, 0, 0, 0, 0, $crop_width, $crop_height, $width, $new_crop_height);
+    set_text_and_save($result_image);
 }
 
-function crop_and_resize($image, $height, $width)
-{
-    //TODO
-    global $cropWidth, $cropHeight;
+function crop_and_resize($image, $height, $width){
+    global $crop_width, $crop_height;
     //count coefficient needed size
-    $coefficient = round($cropHeight / $cropWidth);
-    if ($cropHeight < $cropWidth) {
-        ($coefficient < 1 && $coefficient > 0.5) ? crop_width($image, $height, $width) : crop_height($image, $height, $width);
-    } else {
-        ($coefficient < 1 && $coefficient > 0.5) ? crop_height($image, $height, $width) : crop_width($image, $height, $width);
-    }
+    $coefficient = round($crop_height / $crop_width);
+    ($crop_height < $crop_width) ?
+        (($coefficient < 1 && $coefficient > 0.5) ?
+            crop_width($image, $height, $width) : crop_height($image, $height, $width)) :
+        (($coefficient < 1 && $coefficient > 0.5) ?
+            crop_height($image, $height, $width) : crop_width($image, $height, $width));
 }
 
-$cropHeight = filter_input_('r_height', '');
-$cropWidth = filter_input_('r_width', '');
+$crop_height = filter_input_('r_height', '');
+$crop_width = filter_input_('r_width', '');
 loadFile();
 include "../general/header.php"; ?>
     <div class="right-col">
@@ -121,16 +118,14 @@ include "../general/header.php"; ?>
     <div class="text-content  clearfix">
         <div class="form">
             <form action="index.php" method="post" class="qwerty" enctype="multipart/form-data">
-                <div class="error"><?= $error_message ?></div>
+                <div class="error"><?=$error_message?></div>
                 <div class="labels">
                     <div> Result height:</div>
                     <div> Result width:</div>
                 </div>
                 <div class="inputs">
-                    <input id="r_height" type="text" name="r_height" pattern="^[1-9]+[0-9]*$" required
-                           value="<?= $cropHeight ?>">
-                    <input id="r_width" type="text" name="r_width" pattern="^[1-9]+[0-9]*$" required
-                           value="<?= $cropWidth ?>">
+                    <input type="text" name="r_height" pattern="^[1-9]+[0-9]*$" required value="<?=$crop_height?>">
+                    <input type="text" name="r_width" pattern="^[1-9]+[0-9]*$" required value="<?=$crop_width?>">
                 </div>
                 <input type="file" class="submit" name="file" required>
                 <input type="submit" class="submit" name="input_submit" value="Resize">
