d62249c3081e5ecafe90001f791f5440a315c0f4
Alyona_Vepritskaya <amusing.arctic.fox@gmail.com>
2020-03-25 13:14:19 +0200

    Lab10 release v18

--- general/header.php
+++ general/header.php
@@ -7,16 +7,13 @@
     <link rel="stylesheet" type="text/css" href="style.css">
     <link href="https://fonts.googleapis.com/css?family=Ubuntu|Ubuntu+Mono&display=swap" rel="stylesheet">
     <title>Web_lab2</title>
-    <script>var jsHostLocal = 'http://localhost/';</script>
-    <script>var jsHost0 = 'http://k503labs.ukrdomen.com/535a/Veprytskaya/';</script>
-    <script>var jsHost = 'http://localhost//xml_jquery/'; //TODO</script>
-   <!-- <script>var jsHost2 = 'http://k503labs.ukrdomen.com/535a/Veprytskaya/canvas/';</script>-->
+    <script>var jsHost = 'http://k503labs.ukrdomen.com/535a/Veprytskaya/xml_jquery/';</script>
 </head>
 <header class="head">
     <div class="logo">
         <div>
             <a href="">
-                <img src="http://localhost/images/Smart.png" alt="SMART">
+                <img src="http://k503labs.ukrdomen.com/535a/Veprytskaya/images/Smart.png" alt="SMART">
             </a>
         </div>
         computers & mobile
@@ -27,109 +24,109 @@
         <img class="imggg" src="http://k503labs.ukrdomen.com/535a/Veprytskaya/images/header_bckgr.png" alt="">
         <div class="navigation-bar">
             <div class="item">
-                <a href="../general/index.php">
-                    <img src="../images/more.png" alt=">">
+                <a href="http://k503labs.ukrdomen.com/535a/Veprytskaya/general/index.php">
+                    <img src="http://k503labs.ukrdomen.com/535a/Veprytskaya/images/more.png" alt=">">
                     ГЛАВНАЯ
                 </a>
             </div>
             <div class="item">
-                <a href="../form_on_js/index.php">
-                    <img src="../images/more.png" alt=">">
+                <a href="http://k503labs.ukrdomen.com/535a/Veprytskaya/form_on_js/index.php">
+                    <img src="http://k503labs.ukrdomen.com/535a/Veprytskaya/images/more.png" alt=">">
                     Регистрация js
                 </a>
             </div>
             <div class="item">
-                <a href="../form_on_php/main.php">
-                    <img src="../images/more.png" alt=">">
+                <a href="http://k503labs.ukrdomen.com/535a/Veprytskaya/form_on_php/main.php">
+                    <img src="http://k503labs.ukrdomen.com/535a/Veprytskaya/images/more.png" alt=">">
                     Регистрация php
                 </a>
             </div>
             <div class="item">
-                <a href="../form_on_jquery/index.php">
-                    <img src="../images/more.png" alt=">">
+                <a href="http://k503labs.ukrdomen.com/535a/Veprytskaya/form_on_jquery/index.php">
+                    <img src="http://k503labs.ukrdomen.com/535a/Veprytskaya/images/more.png" alt=">">
                     Регистрация jquery
                 </a>
             </div>
             <div class="item">
-                <a href="../form_on_html5/index.php">
-                    <img src="../images/more.png" alt=">">
+                <a href="http://k503labs.ukrdomen.com/535a/Veprytskaya/form_on_html5/index.php">
+                    <img src="http://k503labs.ukrdomen.com/535a/Veprytskaya/images/more.png" alt=">">
                     Регистрация HTML5
                 </a>
             </div>
             <div class="item">
-                <a href="../redirect_page/index.php">
-                    <img src="../images/more.png" alt=">">
+                <a href="http://k503labs.ukrdomen.com/535a/Veprytskaya/redirect_page/index.php">
+                    <img src="http://k503labs.ukrdomen.com/535a/Veprytskaya/images/more.png" alt=">">
                     Редирект
                 </a>
             </div>
             <div class="item">
-                <a href="../animation/index.php">
-                    <img src="../images/more.png" alt=">">
+                <a href="http://k503labs.ukrdomen.com/535a/Veprytskaya/animation/index.php">
+                    <img src="http://k503labs.ukrdomen.com/535a/Veprytskaya/images/more.png" alt=">">
                     Анимация js
                 </a>
             </div>
             <div class="item">
-                <a href="../animation_css/index.php">
-                    <img src="../images/more.png" alt=">">
+                <a href="http://k503labs.ukrdomen.com/535a/Veprytskaya/animation_css/index.php">
+                    <img src="http://k503labs.ukrdomen.com/535a/Veprytskaya/images/more.png" alt=">">
                     Анимация css
                 </a>
             </div>
             <div class="item">
-                <a href="../server_var/index.php">
-                    <img src="../images/more.png" alt=">">
+                <a href="http://k503labs.ukrdomen.com/535a/Veprytskaya/server_var/index.php">
+                    <img src="http://k503labs.ukrdomen.com/535a/Veprytskaya/images/more.png" alt=">">
                     Содержимое серверных переменных
                 </a>
             </div>
             <div class="item">
-                <a href="../nav_dir/index.php">
-                    <img src="../images/more.png" alt=">">
+                <a href="http://k503labs.ukrdomen.com/535a/Veprytskaya/nav_dir/index.php">
+                    <img src="http://k503labs.ukrdomen.com/535a/Veprytskaya/images/more.png" alt=">">
                     Файловый навигатор
                 </a>
             </div>
             <div class="item">
-                <a href="../xml/index.php">
-                    <img src="../images/more.png" alt=">">
+                <a href="http://k503labs.ukrdomen.com/535a/Veprytskaya/xml/index.php">
+                    <img src="http://k503labs.ukrdomen.com/535a/Veprytskaya/images/more.png" alt=">">
                     XML
                 </a>
             </div>
             <div class="item">
-                <a href="../xml2/index.php">
-                    <img src="../images/more.png" alt=">">
+                <a href="http://k503labs.ukrdomen.com/535a/Veprytskaya/xml2/index.php">
+                    <img src="http://k503labs.ukrdomen.com/535a/Veprytskaya/images/more.png" alt=">">
                     XML_2_ajax
                 </a>
             </div>
             <div class="item">
-                <a href="../xml_jquery/index.php">
-                    <img src="../images/more.png" alt=">">
+                <a href="http://k503labs.ukrdomen.com/535a/Veprytskaya/xml_jquery/index.php">
+                    <img src="http://k503labs.ukrdomen.com/535a/Veprytskaya/images/more.png" alt=">">
                     XML_3_ajax_jQuery
                 </a>
             </div>
             <div class="item">
-                <a href="../carusel_jquery/index.php">
-                    <img src="../images/more.png" alt=">">
+                <a href="http://k503labs.ukrdomen.com/535a/Veprytskaya/carusel_jquery/index.php">
+                    <img src="http://k503labs.ukrdomen.com/535a/Veprytskaya/images/more.png" alt=">">
                     jCarousel
                 </a>
             </div>
             <div class="item">
-                <a href="../table/index.php">
-                    <img src="../images/more.png" alt=">">
+                <a href="http://k503labs.ukrdomen.com/535a/Veprytskaya/table/index.php">
+                    <img src="http://k503labs.ukrdomen.com/535a/Veprytskaya/images/more.png" alt=">">
                     Table
                 </a>
             </div>
             <div class="item">
-                <a href="../canvas/index.php">
-                    <img src="../images/more.png" alt=">">
+                <a href="http://k503labs.ukrdomen.com/535a/Veprytskaya/canvas/index.php">
+                    <img src="http://k503labs.ukrdomen.com/535a/Veprytskaya/images/more.png" alt=">">
                     Histogram
                 </a>
             </div>
             <div class="item">
-                <a href="../resize_img/index.php">
-                    <img src="../images/more.png" alt=">">
+                <a href="http://k503labs.ukrdomen.com/535a/Veprytskaya/resize_img/index.php">
+                    <img src="http://k503labs.ukrdomen.com/535a/Veprytskaya/images/more.png" alt=">">
                     ReSize
                 </a>
             </div>
             <div class="bottom-img">
-                <img src="../images/left-frame3.png" alt="">
+                <img src="http://k503labs.ukrdomen.com/535a/Veprytskaya/images/left-frame3.png" alt="">
             </div>
         </div>
         <div class="items-container">
@@ -140,13 +137,13 @@
             </a>
             <div class="item-image">
                 <a href="#">
-                    <img src="../images/Layer_comer.png" alt="comer">
+                    <img src="http://k503labs.ukrdomen.com/535a/Veprytskaya/images/Layer_comer.png" alt="comer">
                     <div class="link">
                         Comer SP-2001
                     </div>
                 </a>
                 <a href="">
-                    <img src="../images/Layer_mouse.png" alt="mouse">
+                    <img src="http://k503labs.ukrdomen.com/535a/Veprytskaya/images/Layer_mouse.png" alt="mouse">
                     <div class="link">
                         BenQ M301
                     </div>
