f83b3eaf42aafe3b2e0bfe3f0cc72b7374ef6be3
Alyona_Vepritskaya <amusing.arctic.fox@gmail.com>
2020-03-27 20:49:18 +0200

    Lab11 release v6

--- sql/ProductModel.php
+++ sql/ProductModel.php
@@ -30,7 +30,7 @@ class ProductModel
     function get_products($section_id)
     {
         $this->products = array();
-        $sql_select = "select * from " . DBT_PRODUCTS . " where id_section=" . $section_id . ";";
+        $sql_select = "select * from " . DBT_PRODUCTS . " where id_section =" . $section_id . ";";
         $result = $this->mysqli->query($sql_select);
         if ($result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
@@ -49,6 +49,30 @@ class ProductModel
 
     function get_product_params($product_id)
     {
+        $this->params = array();
+        $sql_select = "select * from " . DBT_PRODUCTS . " where s_num ='" . $product_id . "';";
+        $result = $this->mysqli->query($sql_select);
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $this->params['name'] = $row["name"];
+                $this->params['year'] = $row["year"];
+                $this->params['country'] = $row["country"];
+                $this->params['s_num'] = $row["s_num"];
+                $this->params['img'] = $row["img"];
+                $this->params['price'] = $row["price"];
+            }
+        }
+        $sql_select2 = "select * from " . DBT_PARAM . " where id_product ='" . $product_id . "';";
+        $result2 = $this->mysqli->query($sql_select2);
+        if ($result2->num_rows > 0) {
+            while ($row = $result2->fetch_assoc()) {
+                $item = array();
+                $item['name'] = $row["name"];
+                $item['value'] = $row["value"];
+                $this->params['param'][] = $item;
+            }
+        }
+        return $this->params;
     }
 
     function get_product_reviews($product_id)
--- sql/main.php
+++ sql/main.php
@@ -18,13 +18,23 @@ insert_data($products, $mysqli);
 // ------------- select data -----------
 $p_model = new ProductModel($mysqli);
 $sections = $p_model->get_sections();
-$prod = null;
+$all_products = null;
+$one_product = null;
 $current_section = null;
+$current_product = null;
+$q = filter_input_("section", '');
+$id = filter_input_("id", '');
 //get products
 function get_products($section_id)
 {
-    global $p_model, $prod;
-    $prod = $p_model->get_products($section_id);
+    global $p_model, $all_products;
+    $all_products = $p_model->get_products($section_id);
+}
+
+function get_product($prod_id)
+{
+    global $p_model, $one_product;
+    $one_product = $p_model->get_product_params($prod_id);
 }
 
 function filter_input_($name, $default)
@@ -39,11 +49,13 @@ function filter_input_($name, $default)
     return $result;
 }
 
-$q = filter_input_("section", '');
 if (!empty($q)) {
-    $section_id = $q;
-    $current_section = $section_id;
-    get_products($section_id);
+    $current_section = $q;
+    get_products($current_section);
+}
+if (!empty($id)) {
+    $current_product = $id;
+    get_product($current_product);
 }
 ?>
 <div class="right-col">
@@ -60,29 +72,49 @@ if (!empty($q)) {
     <div class="text-content  clearfix">
         <div class="products">
             <?php
-            foreach ($sections as $key => $value) { ?>
-                <a href="index.php?section=<?= $key ?>" class="buy-item"><?= $value ?></a>
-                <?php
-                if ($current_section == $key) {
-                    foreach ($prod as $k => $v) { ?>
-                        <div class="product">
-                            <div class="item-name"><?= $v['name'] ?></div>
-                            <img src="<?= $path . $v['img'] ?>" alt="img">
-                            <div class="description">
-                                <div>Serial number: <?= $v['s_num'] ?></div>
-                                <div>Price: <?= $v['prise'] ?></div>
-                                <div>Production date: <?= $v['year'] ?></div>
-                                <div>Production country: <?= $v['country'] ?></div>
-                                <div class="details">
-                                    <input name="More" class="buy-item more" type="submit" value="More">
+            if (is_null($one_product)) {
+                foreach ($sections as $key => $value) { ?>
+                    <a href="index.php?section=<?= $key ?>" class="buy-item"><?= $value ?></a>
+                    <?php
+                    if ($current_section == $key) {
+                        foreach ($all_products as $k => $v) { ?>
+                            <div class="product">
+                                <div class="item-name"><?= $v['name'] ?></div>
+                                <img src="<?= $path . $v['img'] ?>" alt="img">
+                                <div class="description">
+                                    <div>Serial number: <?= $v['s_num'] ?></div>
+                                    <div>Price: <?= $v['price'] ?></div>
+                                    <div>Production date: <?= $v['year'] ?></div>
+                                    <div>Production country: <?= $v['country'] ?></div>
+                                    <div class="details">
+                                        <a href="index.php?id=<?= $v['s_num'] ?>" class="buy-item more">More</a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <?php
+                            <?php
+                        }
                     }
                 }
-            }
-            ?>
+            } else {
+                ?>
+                <div class="product">
+                    <div class="item-name"><?= $one_product['name'] ?></div>
+                    <img src="<?= $path . $one_product['img'] ?>" alt="img">
+                    <div class="description">
+                        <div>Serial number: <?= $one_product['s_num'] ?></div>
+                        <div>Price: <?= $one_product['price'] ?></div>
+                        <div>Production date: <?= $one_product['year'] ?></div>
+                        <div>Production country: <?= $one_product['country'] ?></div>
+                        <?php foreach ($one_product['param'] as $key => $value) { ?>
+                            <div><?= $value['name'] . " : " . $value['value'] ?></div>
+                        <?php } ?>
+                        <div class="details">
+                            <input type="button" class="buy-item more" value="Add to card">
+                        </div>
+                    </div>
+                </div>
+                <?php
+            } ?>
         </div>
     </div>
 
